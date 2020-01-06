26201409ecad20ec60d1f2b94a5979273e3121ed
TharinduDR <rhtdranasinghe@gmail.com>
2020-01-06 15:11:37 +0000

    007: Modified the model implementation

--- algo/neural_nets/models/rnn/model.py
+++ algo/neural_nets/models/rnn/model.py
@@ -20,15 +20,19 @@ class RNN(nn.Module):
         self.embedding.weight.data.copy_(pretrained_embeddings)
         self.embedding.weight.requires_grad = False
 
-        self.gru = nn.GRU(embedding_dim, HIDDEN_DIM, num_layers=N_LAYERS,
-                          bidirectional=BIDIRECTIONAL, dropout=DROPOUT)
-        self.fc = nn.Linear(HIDDEN_DIM * 2, output_dim)
+        self.rnn = nn.GRU(embedding_dim, HIDDEN_DIM, num_layers=N_LAYERS,
+                          bidirectional=BIDIRECTIONAL, dropout=0 if N_LAYERS < 2 else DROPOUT)
+        self.fc = nn.Linear(HIDDEN_DIM * 2 if BIDIRECTIONAL else HIDDEN_DIM, output_dim)
         self.dropout = nn.Dropout(DROPOUT)
 
     def forward(self, x):
         x = self.embedding(x)
         embedded = self.dropout(x)
-        output_gru, hidden = self.gru(embedded)
-        hidden = self.dropout(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))
+        output_gru, hidden = self.rnn(embedded)
+
+        if self.rnn.bidirectional:
+            hidden = self.dropout(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))
+        else:
+            hidden = self.dropout(hidden[-1, :, :])
 
         return self.fc(hidden.squeeze(0))
