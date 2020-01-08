8d5ea1d161cb317a0bf95fcbf9268c734e20ce14
TharinduDR <rhtdranasinghe@gmail.com>
2020-01-08 12:19:41 +0000

    009: Added visualisations

--- project_config.py
+++ project_config.py
@@ -1,3 +1,6 @@
 SEED = 77
-ENGLISH_DATA_PATH = "/English/olid-training-v1.0.tsv"
+ENGLISH_DATA_PATH = "/home/tharindu/Projects/OffensEval_2020/data/English/olid-training-v1.0.tsv"
+DANISH_DATA_PATH = "/home/tharindu/Projects/OffensEval_2020/data/Danish/offenseval-da-training-v1.tsv"
+GREEK_DATA_PATH = "/home/tharindu/Projects/OffensEval_2020/data/Greek/offenseval-greek-training-v1.tsv"
+TURKISH_DATA_PATH = "/home/tharindu/Projects/OffensEval_2020/data/Turkish/offenseval-tr-training-v1.tsv"
 VECTOR_CACHE = "/home/tharindu/Projects/OffensEval_2020/cache"
---
+++ visualisation.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from nltk.corpus import stopwords
+from wordcloud import WordCloud, STOPWORDS
+
+greek_stopwords = set(stopwords.words('greek'))
+
+
+def plot_data(df, class_name):
+    ax = df[class_name].value_counts().plot(kind='bar',
+                                            figsize=(12, 8),
+                                            title="Class Distribution")
+    total = 0
+    for p in ax.patches:
+        total += p.get_height()
+    for p in ax.patches:
+        ax.annotate(np.round(p.get_height(), decimals=2), (p.get_x() + p.get_width() / 2, p.get_height() / 2),
+                    ha='center', va='center', xytext=(0, 10), textcoords='offset points',
+                    bbox=dict(facecolor='yellow', alpha=0.5))
+
+        ax.annotate(str(np.round(p.get_height() / total * 100, decimals=2)) + "%",
+                    (p.get_x() + p.get_width() / 2, p.get_height() / 2),
+                    ha='center', va='center', xytext=(0, -20), textcoords='offset points',
+                    bbox=dict(facecolor='gray', alpha=0.5))
+
+
+def plot_word_cloud(df):
+    text = df.tweet.values
+    wordcloud = WordCloud(
+        width=3000,
+        height=2000,
+        background_color='black',
+        stopwords=greek_stopwords).generate(str(text))
+    fig = plt.figure(
+        figsize=(40, 30),
+        facecolor='k',
+        edgecolor='k')
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.tight_layout(pad=0)
+    plt.show()
+
