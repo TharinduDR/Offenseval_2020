285901d76aff7af9a98539cd5542197fa76d4824
TharinduDR <rhtdranasinghe@gmail.com>
2020-02-27 15:38:56 +0000

    027: Fixed a bug in Arabic reading file

---
+++ util/reader.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+def read_test_tsv(path):
+    id = []
+    tweet = []
+    with open(path, 'rt') as file_in:
+        next(file_in)
+        for line in file_in:
+            components = line.split(sep="\t")
+            id.append(components[0])
+            tweet.append(components[1])
+
+    return pd.DataFrame(
+        {'id': id,
+         'tweet': tweet
+         })
+
+
+def read_train_tsv(path):
+    id = []
+    tweet = []
+    label = []
+    with open(path, 'rt') as file_in:
+        next(file_in)
+        for line in file_in:
+            components = line.split(sep="\t")
+            id.append(components[0])
+            tweet.append(components[1])
+            label.append(components[2])
+
+    return pd.DataFrame(
+        {'id': id,
+         'tweet': tweet,
+         'subtask_a': label
+         })
